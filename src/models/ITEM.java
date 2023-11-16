6eb8702bb9126559a527115ae8a29a6920a82c86
Mighty Doffoe <mdoffoe@eagles.bridgewater.edu>
2023-11-16 11:33:29 -0500

    Enum class for items

---
+++ src/models/ITEM.java
@@ -0,0 +1,19 @@
+public enum ITEM{
+
+	stick(5),
+	sword(10),
+	potion(0),
+	key(0);
+
+// in order to get the damage values of each of the items, you have to call: ITEM.item_name.getDamge();  	
+ 	private int damage;
+ 	ITEM (int d){
+ 		damage = d;
+ 	}
+
+	public int getDamage(){
+ 		return damage;
+ 	}
+}
+
+
